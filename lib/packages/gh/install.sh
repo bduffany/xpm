80d8bb541ef3abd3d71f687adca56a31af85a3e7
Brandon Duffany <brandon@buildbuddy.io>
2021-02-20 03:40:48 -0500

    Use apt if present

--- lib/packages/gh/install.sh
+++ lib/packages/gh/install.sh
@@ -3,6 +3,14 @@ if [[ "$_XPM_KERNEL" == "darwin" ]]; then
   exit
 fi
 
+if which apt &>/dev/null; then
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key C99B11DEB97541F0
+  sudo apt-add-repository https://cli.github.com/packages
+  sudo apt update
+  sudo apt install gh
+  exit
+fi
+
 download_url="$(
   curl -sS https://api.github.com/repos/cli/cli/releases/latest |
     jq -r '.assets[] | .browser_download_url | select(endswith("linux_amd64.tar.gz"))'
