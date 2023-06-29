29a0b8fce7ccf7688588628ca71615e3a07c47ba
Brandon Duffany <brandon@buildbuddy.io>
2023-06-28 20:34:21 -0400

    Fix ngrok

--- lib/packages/ngrok/install.sh
+++ lib/packages/ngrok/install.sh
@@ -1,5 +1,5 @@
 variant="$_XPM_KERNEL-amd64"
 download_url="$(curl -s 'https://ngrok.com/download' | grep -oP 'href="https.*?'"$variant"'.*?"' | sed -r 's/href="(.*?)"/\1/g')"
-wget -O ngrok.zip "$download_url"
-unzip ngrok.zip
+wget -O ngrok.tar.gz "$download_url"
+tar -xvf ngrok.tar.gz
 sudo cp ngrok "$_XPM_LOCAL_BIN_PATH/ngrok"
