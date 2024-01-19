d87203870fe7b133d58aceac6cbded8b321a3464
Brandon Duffany <brandon@buildbuddy.io>
2024-01-19 08:14:00 -0500

    Add wezterm

---
+++ lib/packages/wezterm/install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+if [[ "$OSTYPE" == darwin* ]]; then
+  echo >&2 "wezterm: not yet supported on macOS"
+  exit 1
+fi
+
+curl -fsSL https://apt.fury.io/wez/gpg.key |
+  sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' |
+  sudo tee /etc/apt/sources.list.d/wezterm.list
+sudo apt-get update
+sudo apt-get install -y wezterm-nightly
